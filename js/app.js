992126d6c0f64d0767d6df39301de5a2d813fb09
harshapatil7 <108178685+harshapatil7@users.noreply.github.com>
2023-09-26 19:11:30 +0530

    Update app.js
    all because of manoj

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ document.addEventListener('mousemove', e => {
 })
 
 document.getElementById("btn").addEventListener("click", () => {
-	window.location.replace = "https://harshapatil7.github.io/Portfolio/index.html";
+	window.location.href = "https://harshapatil7.github.io/Portfolio/index.html";
 });
 
 
